be3376d97a79d05dd945a7637529e781f20f0869
Levi Cai <lcai@whoi.edu>
2025-03-30 23:05:28 -0400

    Fixes for gym registration bug

--- __init__.py
+++ __init__.py
@@ -11,8 +11,6 @@ import gymnasium as gym
 
 from . import agents
 from .warpauv_env import WarpAUVEnv, WarpAUVEnvCfg
-from .warpauv_vel_env import WarpAUVVelEnvCfg, WarpAUVVelEnv
-from .warpauv_stabilize_env import WarpAUVStabilizeEnvCfg, WarpAUVStabilizeEnv
 
 ##
 # Register Gym environments.
@@ -20,30 +18,10 @@ from .warpauv_stabilize_env import WarpAUVStabilizeEnvCfg, WarpAUVStabilizeEnv
 
 gym.register(
     id="Isaac-WarpAUV-Direct-v1",
-    entry_point="omni.isaac.lab_tasks.direct.learning-based-control:WarpAUVEnv",
+    entry_point="omni.isaac.lab_tasks.direct.isaac-auv-env:WarpAUVEnv",
     disable_env_checker=True,
     kwargs={
         "env_cfg_entry_point": WarpAUVEnvCfg,
-        "rsl_rl_cfg_entry_point": agents.rsl_rl_ppo_cfg.WarpAUVPPORunnerCfg
-    },
-)
-
-gym.register(
-    id="Isaac-WarpAUV-Velocity-Direct-v1",
-    entry_point="omni.isaac.lab_tasks.direct.learning-based-control:WarpAUVVelEnv",
-    disable_env_checker=True,
-    kwargs={
-        "env_cfg_entry_point": WarpAUVVelEnvCfg,
-        "rsl_rl_cfg_entry_point": agents.rsl_rl_ppo_cfg.WarpAUVPPORunnerCfg
-    },
-)
-
-gym.register(
-    id="Isaac-WarpAUV-Stabilize-Direct-v1",
-    entry_point="omni.isaac.lab_tasks.direct.learning-based-control:WarpAUVStabilizeEnv",
-    disable_env_checker=True,
-    kwargs={
-        "env_cfg_entry_point": WarpAUVStabilizeEnvCfg,
         "rsl_rl_cfg_entry_point": agents.rsl_rl_ppo_cfg.WarpAUVPPORunnerCfg
     },
 )
